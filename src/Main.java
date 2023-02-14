8cb963ee559bf24ce7d08d65d5d479427af429df
FlorinCP <92251123+FlorinCP@users.noreply.github.com>
2023-02-14 23:45:50 +0200

    347 Top K
    varianta cu PriorityQueue
    https://www.youtube.com/watch?v=2uCm0j5aeq0&ab_channel=codinginterviewquestions

--- src/Main.java
+++ src/Main.java
@@ -7,48 +7,28 @@ public class Main {
     public static void main(String[] args) {
         int k=2;
         int [] nums ={1,1,1,2,2,3};
-        // Aici adaugam valorile in hashmap
-       Map<Integer,Integer> bucket = new HashMap<>();
-        for (int num:nums) {
-            // metoda consacrata de a adauga mai multe valori
-            bucket.put(num,bucket.getOrDefault(num,0)+1);
-        }
-        System.out.println(bucket);
+        int answer[] = new int[k];
+       HashMap<Integer,Integer> hashMap = new HashMap<>();
+       // hmap key -> numarul si value -> conut
+        // priority queue to sort , implementarea heap ului
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->Integer.compare(hashMap.get(a),hashMap.get(b)));
 
-        // Aici am schimbat valorile de la o galeata la alta
-        Map<Integer,List> bucket2 = new HashMap<>();
-        for (Integer i: bucket.keySet()) {
-            Integer elemFreq = bucket.get(i);
-            if(!bucket2.containsKey(elemFreq)){
-                bucket2.put(elemFreq,new ArrayList<>());
+        // aici populam hm si get or Default e foarte jmekera metoda , daca e prima inserare pune 0 , daca nu valoarea precedenta +1
+        for (int i:nums) {
+            hashMap.put(i, hashMap.getOrDefault(i,0)+1);
+        }
+
+        for (Integer i : hashMap.keySet()) {
+            pq.add(i);
+
+            if(pq.size()>k){
+                pq.poll();
             }
-            bucket2.get(elemFreq).add(i);
         }
 
-        System.out.println(bucket2);
-
-        // Acum luam elementele de sus 
-        int[] result = new int[k];
-        // Stim ca lugimea array-ului este maximul de frecventa pe care il avem
-        for (int i = nums.length; i>0 ; i--) {
-            if(bucket2.containsKey(i)){
-                // aici punem intr un array elementele pt care avem frecventa respectiva , pt ca putem avea mai multe elemente cu aceiasi frecventa
-                List<Integer> list = bucket2.get(i);
-                for (Integer j: list) {
-                    result[--k]=j;
-                    if(k==0){
-                        return result;
-                    }
-                }
-            }
-            
+        for (int i = 0; i < k; i++) {
+            answer[i] = pq.poll();
         }
-
-
-
-
-
-
 
     }
 
